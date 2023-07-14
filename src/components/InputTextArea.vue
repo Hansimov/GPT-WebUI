b8a8fb90078ce7be08d9f82104d28ab8b5b1f521
Hansimov <591172499@qq.com>
2023-07-15 02:21:44 +0800

    [UI] InputTextArea padding, line-height, and character counts

--- src/components/InputTextArea.vue
+++ src/components/InputTextArea.vue
@@ -1,3 +1,38 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      message: '',
+      characterLimit: 10000
+    }
+  }
+})
+</script>
+
 <template>
-  <input v-model="message" placeholder="edit me" />
+  <div class="input-container">
+    <input v-model="message" placeholder="prompt" />
+    <div class="character-count">{{ message.length }} / {{ characterLimit }}</div>
+  </div>
 </template>
+
+<style scoped>
+.input-container {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 10px;
+}
+
+.input-container input {
+  line-height: 2;
+  width: 100%;
+}
+
+.character-count {
+  text-align: left;
+}
+</style>
