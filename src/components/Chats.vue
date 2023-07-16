f6bc2204acae5cf31a84eb5c0ea66a803aa8ad88
Hansimov <591172499@qq.com>
2023-07-17 02:03:35 +0800

    [Refactor] Data structure of LLM Configs based on models

--- src/components/Chats.vue
+++ src/components/Chats.vue
@@ -1,59 +1,66 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-interface ConfigsOfRole {
-  [key: string]: string
+
+interface LLMConfig {
+  name: string
+  class: string
+  avatar: string
+}
+
+interface LLMConfigs {
+  [key: string]: LLMConfig
 }
 
 export default defineComponent({
   data: () => ({
     messages: [
       {
+        model: 'user',
         role: 'user',
         content: 'What is your model?'
       },
       {
-        role: 'gpt-4',
+        model: 'gpt-4',
+        role: 'llm',
         content:
           "I am based on the GPT-4 model, which is a language model developed by OpenAI. GPT stands for Generative Pre-trained Transformer, and the '4' refers to the fourth iteration of this model series. Like its predecessors, GPT-4 is trained on a diverse range of internet text, but it's also fine-tuned with specific datasets to perform certain tasks."
       },
       {
-        role: 'gpt-3.5',
+        model: 'gpt-3.5',
+        role: 'llm',
         content: 'I am GPT-3.5'
       },
       {
-        role: 'claude-2',
+        model: 'claude-2',
+        role: 'llm',
         content: 'I am Claude-2'
       }
     ],
-    class_of_role: {
-      user: 'user-chats',
-      'gpt-3.5': 'gpt-35-chats',
-      'gpt-4': 'gpt-4-chats',
-      'claude-2': 'Claude2-chats'
-    } as ConfigsOfRole,
-    avatar_of_role: {
-      user: 'src/assets/user.png',
-      'claude-2': 'src/assets/claude.png',
-      'gpt-3.5': 'src/assets/gpt-3.5.png',
-      'gpt-4': 'src/assets/gpt-4.png'
-    } as ConfigsOfRole,
-    display_name_of_role: {
-      user: 'User',
-      claude2: 'Claude-2',
-      'gpt-3.5': 'GPT-3.5',
-      'gpt-4': 'GPT-4'
-    }
+    llm_configs: {
+      user: {
+        name: 'User',
+        class: 'user-chats',
+        avatar: 'src/assets/user.png'
+      },
+      'gpt-3.5': {
+        name: 'GPT-3.5',
+        class: 'gpt-35-chats',
+        avatar: 'src/assets/gpt-3.5.png'
+      },
+      'gpt-4': {
+        name: 'GPT-4',
+        class: 'gpt-4-chats',
+        avatar: 'src/assets/gpt-4.png'
+      },
+      'claude-2': {
+        name: 'Claude-2',
+        class: 'claude-2-chats',
+        avatar: 'src/assets/claude-2.png'
+      }
+    } as LLMConfigs
   }),
   computed: {},
-  methods: {
-    async getAvatar(role: string) {
-      const avatarPath = this.avatar_of_role[role]
-      if (avatarPath) {
-        const module = await import(avatarPath)
-        return module.default
-      }
-    }
-  }
+  methods: {}
 })
 </script>
 
@@ -64,12 +71,15 @@ export default defineComponent({
       fluid
       v-for="(message, idx) in messages"
       :key="idx"
-      :class="class_of_role[message.role]"
+      :class="llm_configs[message.model]['class']"
     >
       <v-row align="center">
         <v-col cols="auto">
           <v-avatar>
-            <v-img :src="avatar_of_role[message.role]" :alt="message.role" />
+            <v-img
+              :src="llm_configs[message.model]['avatar']"
+              :alt="llm_configs[message.model]['name']"
+            />
           </v-avatar>
         </v-col>
         <v-col><v-card variant="text" :text="message.content"></v-card></v-col>
@@ -88,7 +98,7 @@ export default defineComponent({
 .gpt-4-chats {
   background-color: lightcyan;
 }
-.Claude2-chats {
+.claude-2-chats {
   background-color: lightyellow;
 }
 </style>
