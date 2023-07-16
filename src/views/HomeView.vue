d137066424d162ffb6abeab7ca090ea062697de2
Hansimov <591172499@qq.com>
2023-07-16 15:01:21 +0800

    [Feature] Use v-icon to control sidebar visibility

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,78 +6,41 @@ import UserInput from '@/components/UserInput.vue'
 export default defineComponent({
   components: {
     UserInput
-  }
+  },
+  data: () => ({
+    left_side_bar_visible: false,
+    right_side_bar_visible: false
+  })
 })
 </script>
-<!-- 
-<template>
-  <v-container>
-    <v-row>
-      <v-col cols="3">
-        <ChatHistory class="chat-history" />
-      </v-col>
-      <v-col cols="3">
-        <div class="messages">Hello World!</div>
-      </v-col>
-      <v-col cols="6">
-        <UserInput class="user-input" />
-      </v-col>
-    </v-row>
-  </v-container>
-</template>
--->
 
 <template>
   <v-app id="inspire">
-    <v-system-bar>
-      <v-spacer></v-spacer>
-
-      <v-icon>mdi-square</v-icon>
-
-      <v-icon>mdi-circle</v-icon>
-
-      <v-icon>mdi-triangle</v-icon>
-    </v-system-bar>
-
-    <v-navigation-drawer color="grey-lighten-3" rail>
-      <v-avatar class="d-block text-center mx-auto mt-4" color="grey-darken-1" size="36"></v-avatar>
-
-      <v-divider class="mx-3 my-5"></v-divider>
-
-      <v-avatar
-        v-for="n in 6"
-        :key="n"
-        class="d-block text-center mx-auto mb-9"
-        color="grey-lighten-1"
-        size="28"
-      ></v-avatar>
-    </v-navigation-drawer>
-
-    <v-navigation-drawer width="244">
-      <v-sheet color="grey-lighten-5" height="128" width="100%"></v-sheet>
-
-      <v-list>
-        <v-list-item v-for="n in 5" :key="n" :title="`Item ${n}`" link> </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar class="px-3" color="grey-lighten-4" flat height="72">
-      <v-spacer></v-spacer>
-
-      <v-responsive max-width="156">
-        <v-text-field
-          bg-color="grey-lighten-2"
-          class="rounded-pill overflow-hidden"
-          density="compact"
-          hide-details
-          variant="solo"
-        ></v-text-field>
-      </v-responsive>
+    <v-navigation-drawer v-model="left_side_bar_visible"> </v-navigation-drawer>
+    <v-app-bar>
+      <v-icon
+        center
+        size="large"
+        icon="$vuetify"
+        color="cyan"
+        start="true"
+        end="true"
+        @click="left_side_bar_visible = !left_side_bar_visible"
+      ></v-icon>
+      <v-icon
+        center
+        size="large"
+        icon="fa fa-user"
+        color="green"
+        start="true"
+        end="true"
+        @click="right_side_bar_visible = !right_side_bar_visible"
+      ></v-icon>
     </v-app-bar>
 
     <v-main>Hello World</v-main>
 
-    <v-navigation-drawer location="right">
+    <v-navigation-drawer location="right" v-model="right_side_bar_visible">
       <v-list>
         <v-list-item v-for="n in 5" :key="n" :title="`Item ${n}`" link> </v-list-item>
       </v-list>
